60ed3780754fb79ea4872883dbf5b8e9b304cf9a
Lorenzo Cinque <lore.cinque@gmail.com>
2018-12-09 21:31:18 +0000

    Create server and db connection.

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// This file connects to remote Prisma DB and give us the ability to query it via JS.
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,17 @@
 require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
 
-console.log(`test dotenv: ${process.env.TEST}`);
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  ({ port }) => {
+    console.log(`Server is now running on port http://localhost:${port}`);
+  }
+);
