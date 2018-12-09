89a2a47c07d9739f92f9fd854c690d1ff3f48d4a
Lorenzo Cinque <lore.cinque@gmail.com>
2018-12-09 22:39:02 +0000

    Create signup mutation

---
+++ backend/src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PASSWORD_SALT_LENGTH: 10,
+  COOKIE_MAX_AGE: 1000 * 60 * 60 * 24 * 365 // 1 year
+};
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,31 @@
-const Mutations = {};
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { PASSWORD_SALT_LENGTH, COOKIE_MAX_AGE } = require('../config');
+
+const Mutations = {
+  async signup(_parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcryptjs.hash(args.password, PASSWORD_SALT_LENGTH);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password
+        }
+      },
+      info
+    );
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie('token', token, {
+      // Deny cookies access from js
+      httpOnly: true,
+      maxAge: COOKIE_MAX_AGE
+    });
+
+    return user;
+  }
+};
 
 module.exports = Mutations;
