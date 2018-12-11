d7e8a2fd0c9b81496a87550c1537866b65f57819
Lorenzo Cinque <lore.cinque@gmail.com>
2018-12-11 21:28:34 +0000

    Start styling base page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,28 @@
+import styled from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 import initNProgress from '../utils/nprogress';
 
 initNProgress();
 
+const StyledPaged = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Content = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+  background: red;
+`;
+
 const Page = props => (
-  <div>
+  <StyledPaged>
     <Meta />
     <Header />
-    {props.children}
-  </div>
+    <Content>{props.children}</Content>
+  </StyledPaged>
 );
 
 export default Page;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,1 @@
-export default () => <div />;
+export default () => <div>Home Page</div>;
