0f86445403ea6691bdf3e835c6d247cdba5a831d
Lorenzo Cinque <lore.cinque@gmail.com>
2018-12-11 21:43:15 +0000

    Add ThemeProvider with a basic theme

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,28 +1,37 @@
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 import initNProgress from '../utils/nprogress';
 
 initNProgress();
 
+const theme = {
+  green: '#2ecc71',
+  black: '#393939',
+  maxWidth: '1000px'
+};
+
+// XXX: We can leave these styled components here
+// since these are used only in this file.
 const StyledPaged = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Content = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
-  background: red;
 `;
 
 const Page = props => (
-  <StyledPaged>
-    <Meta />
-    <Header />
-    <Content>{props.children}</Content>
-  </StyledPaged>
+  <ThemeProvider theme={theme}>
+    <StyledPaged>
+      <Meta />
+      <Header />
+      <Content>{props.children}</Content>
+    </StyledPaged>
+  </ThemeProvider>
 );
 
 export default Page;
