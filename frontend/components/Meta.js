b6dadc54e12d3c0ddf95df00ff3956910a365473
Lorenzo Cinque <lore.cinque@gmail.com>
2018-12-11 21:37:40 +0000

    Add favicon :)

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -4,6 +4,7 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <link rel="shortcut icon" href="/static/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <title>PhotoRice</title>
   </Head>
