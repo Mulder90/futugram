4d1ea9091a89a3742d02808a37f305ab2896ca79
Lorenzo Cinque <lore.cinque@gmail.com>
2018-12-13 21:04:18 +0000

    Add PROD_API_ENDPOINT and DEV_API_ENDPOINT and test deploy on Heroku

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,3 @@
-export const API_ENDPOINT = 'http://localhost:4000';
+export const DEV_API_ENDPOINT = 'http://localhost:4000';
+export const PROD_API_ENDPOINT = 'https://futugramapp.herokuapp.com/';
 export const APP_NAME = 'futugram';
--- frontend/utils/withData.js
+++ frontend/utils/withData.js
@@ -1,10 +1,13 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
-import { API_ENDPOINT } from '../config';
+import { DEV_API_ENDPOINT, PROD_API_ENDPOINT } from '../config';
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: API_ENDPOINT,
+    uri:
+      process.env.NODE_ENV === 'development'
+        ? DEV_API_ENDPOINT
+        : PROD_API_ENDPOINT,
     request: operation => {
       operation.setContext({
         fetchOptions: {
